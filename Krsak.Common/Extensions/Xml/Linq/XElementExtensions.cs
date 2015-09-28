3b4795cb5707461e3ff1df266f5ffe35dcadf8d3
kurusu.akira <krs.aki@gmail.com>
2015-09-28 11:33:39 +0900

    【追加】XElementからXPathを生成する拡張メソッドを追加

---
+++ Krsak.Common/Extensions/Xml/Linq/XElementExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace Krsak.Common.Extensions.Xml.Linq
+{
+	public static class XElementExtensions
+	{
+		private static readonly Dictionary<string, string> DicStrStrEmpty = new Dictionary<string, string>();
+		/// <summary><see cref="XElement"/>からXPathを生成する。同一階層に同名要素が複数あるケースは未対応
+		/// </summary>
+		/// <param name="_this"></param>
+		/// <param name="dicElemName_KeyAttrName"></param>
+		/// <returns></returns>
+		public static string GetXPath(this XElement _this, Dictionary<string, string> dicElemName_KeyAttrName = null)
+		{
+			var dic_elem_name__key_attr_name = dicElemName_KeyAttrName ?? DicStrStrEmpty;
+			var elem_names = new List<string>();
+
+			var elem = _this;
+			while (elem != null) {
+				string nmspace = elem.GetPrefixOfNamespace(elem.Name.Namespace);
+				string elem_name = (string.IsNullOrEmpty(nmspace) ? "" : nmspace + ":") + elem.Name.LocalName;
+				string xpath_elem_name = elem_name;
+				string key_attr_name = null;
+				if (dic_elem_name__key_attr_name.TryGetValue(elem_name, out key_attr_name)) {
+					var attr = elem.Attribute(key_attr_name);
+					if (attr != null) {
+						xpath_elem_name += $"[@{attr.Name.LocalName}='{attr.Value}']";
+					}
+				}
+				elem_names.Add(xpath_elem_name);
+				elem = elem.Parent;
+			}
+
+			elem_names.Add(""); // ルートノード用
+			return string.Join("/", ((IEnumerable<string>)elem_names).Reverse());
+		}
+	}
+}
---
+++ Krsak.CommonTests/Extensions/Xml/Linq/XElementExtensionsTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Krsak.Common.Extensions.Xml.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace Krsak.Common.Extensions.Xml.Linq.Tests
+{
+	[TestClass()]
+	public class XElementExtensionsTests
+	{
+		[TestMethod()]
+		[TestCategory("XElementExtensions")]
+		public void GetXPathTest1()
+		{
+			string str_xml = @"
+<Item id='xxx'>
+  <i18n:text xml:lang='ja' xmlns:i18n='http://www.aras.com/I18N'>あああ</i18n:text>
+  <i18n:text xml:lang='en' xmlns:i18n='http://www.aras.com/I18N'>aaa</i18n:text>
+</Item>
+";
+			var doc = XDocument.Parse(str_xml);
+			var nsmgr = new XmlNamespaceManager(new NameTable());
+			nsmgr.AddNamespace("i18n", "http://www.aras.com/I18N");
+			nsmgr.AddNamespace("xml", "http://www.w3.org/XML/1998/namespace");
+			var elem = doc.XPathSelectElement("/Item/i18n:text", nsmgr);
+			var xpath = elem.GetXPath(new Dictionary<string, string>() { { "Item", "id" }, });
+
+			xpath.Is(@"/Item[@id='xxx']/i18n:text");
+		}
+
+		[TestMethod()]
+		[TestCategory("XElementExtensions")]
+		public void GetXPathTest2()
+		{
+			string str_xml = @"
+<AML>
+<Item id='yyy'>
+  <i18n:text xml:lang='ja' xmlns:i18n='http://www.aras.com/I18N'>あああ</i18n:text>
+  <i18n:text xml:lang='en' xmlns:i18n='http://www.aras.com/I18N'>aaa</i18n:text>
+</Item>
+</AML>
+";
+			var doc = XDocument.Parse(str_xml);
+			var nsmgr = new XmlNamespaceManager(new NameTable());
+			nsmgr.AddNamespace("i18n", "http://www.aras.com/I18N");
+			nsmgr.AddNamespace("xml", "http://www.w3.org/XML/1998/namespace");
+			var elem = doc.XPathSelectElement("/AML/Item/i18n:text", nsmgr);
+			var xpath = elem.GetXPath(new Dictionary<string, string>() { { "Item", "id" }, });
+
+			xpath.Is(@"/AML/Item[@id='yyy']/i18n:text");
+		}
+
+		[TestMethod()]
+		[TestCategory("XElementExtensions")]
+		public void GetXPathTest3()
+		{
+			string str_xml = @"
+<AML>
+<Item id='xxx'>
+  <i18n:text xml:lang='ja' xmlns:i18n='http://www.aras.com/I18N'>あああ</i18n:text>
+  <i18n:text xml:lang='en' xmlns:i18n='http://www.aras.com/I18N'>aaa</i18n:text>
+  <Relationships>
+   <Item id='yyy'>
+	 <i18n:label xml:lang='ja' xmlns:i18n='http://www.aras.com/I18N'>いいい</i18n:label>
+	 <i18n:label xml:lang='en' xmlns:i18n='http://www.aras.com/I18N'>iii</i18n:label>
+	 <related_id>
+	  <Item id='zzz'>
+		<i18n:description xml:lang='ja' xmlns:i18n='http://www.aras.com/I18N'>ううう</i18n:description>
+		<i18n:description xml:lang='en' xmlns:i18n='http://www.aras.com/I18N'>uuu</i18n:description>
+	  </Item>
+	 </related_id>
+   </Item>
+  </Relationships>
+</Item>
+</AML>
+";
+			var doc = XDocument.Parse(str_xml);
+			var nsmgr = new XmlNamespaceManager(new NameTable());
+			nsmgr.AddNamespace("i18n", "http://www.aras.com/I18N");
+			nsmgr.AddNamespace("xml", "http://www.w3.org/XML/1998/namespace");
+			var elem = doc.XPathSelectElement("/AML/Item/Relationships/Item/related_id/Item/i18n:description", nsmgr);
+			var xpath = elem.GetXPath(new Dictionary<string, string>() { { "Item", "id" }, });
+
+			xpath.Is(@"/AML/Item[@id='xxx']/Relationships/Item[@id='yyy']/related_id/Item[@id='zzz']/i18n:description");
+		}
+
+	}
+}
